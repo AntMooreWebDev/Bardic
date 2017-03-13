f285ffe644cb89a0326b382be3f02b38900af604
Ant <antoniomoore.am91@gmail.com>
2017-03-13 14:00:21 +0000

    Completed basic support for AJAX and getJSON shorthand

--- src/Bardic.js
+++ src/Bardic.js
@@ -1,51 +1,93 @@
 var Bardic = (function() {
-    function RootParams(params) {
+    function AJAXParams(params) {
          this.url = params.url || "";
          this.onSuccess = params.onSuccess || function(){};
-         this.includeDebugData || false;
+         this.onError = params.onError || function(){};
+         this.includeDebugData = params.includeDebugData || false;
          this.type = params.type || "GET";
     }
 
+    function JSONParams(url, onSuccess, includeDebugData) {
+        this.url = url || "";
+        this.onSuccess = onSuccess || function(){};
+        this.includeDebugData = includeDebugData || false;
+        this.type = "GET";
+    }
+
+    function openRequest(type_, url) {
+        var request = new XMLHttpRequest();
+        request.open(type_, url, true);
+
+        return request;
+    }
+
+    function getErrorData(params, request) {
+        var data = {};
+
+        if (params.includeDebugData) {
+            data.DebugData = {
+                request: request
+            }
+            console.error(request.status + ": " + request.statusText, data);
+        }
+
+        if (params.onError) {
+            params.onError(request, request.statusText);
+        }
+    }
+
     return {
-        getJSON: function(params) {
-            params.type = "GET";
-            this.ajax(params);
+        getJSON: function(url, onSuccess, includeDebugData) {
+            var Params = new JSONParams(url, onSuccess, includeDebugData);
+            var request = openRequest(Params.type, url);
+
+            request.onload = function() {
+                if (request.status >= 200 && request.status < 400) {
+                    var data = JSON.parse(request.responseText);
+
+                    if (Params.includeDebugData) {
+                        data.DebugData = {
+                            request: request
+                        }
+                    }
+
+                    Params.onSuccess(data);
+                } else {
+                    getErrorData(Params, request);
+                }
+            }
+
+            request.onerror = function() {
+                getErrorData(Params, request);
+            };
+
+            request.send();
         },
         ajax: function(params) {
-            var rootParams = new RootParams(params);
-
-            var request = new XMLHttpRequest();
-            request.open(rootParams.type, rootParams.url, true);
+            var Params = new AJAXParams(params);
+            var request = openRequest(Params.type, Params.url, true);
 
             request.onload = function() {
                 if (request.status >= 200 && request.status < 400) {
                     var data = JSON.parse(request.responseText);                    
                     
-                    if (rootParams.includeDebugData) {
+                    if (Params.includeDebugData) {
                     	data.DebugData = {
                         	response: request
                         }
                     }
                     
-                    rootParams.onSuccess(data);
+                    Params.onSuccess(data);
                 } else {
-                    // We reached our target server, but it returned an error
+                    getErrorData(Params, request);
                 }
+            }
+
+            request.onerror = function() {
+                getErrorData(Params, request);
             }
 
             request.send();
         }
     }
 })();
-
-
-/*
-
-Bardic.getJSON({
-	url: "https://api.myjson.com/bins/146swf",
-	onSuccess: function(data) {
-		console.log("returned data", data);
-	}
-});
-
-*/
